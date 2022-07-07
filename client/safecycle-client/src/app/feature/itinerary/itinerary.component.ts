ca53245418402254a5fdd904c2235080d3050bee
William D'Andrea <williapile@gmail.com>
2022-07-07 12:02:52 +0200

    [FRONT-END] - Creating the itinerary page with the map - No-scroll page implementation in CSS

--- client/safecycle-client/src/app/feature/itinerary/itinerary.component.ts
+++ client/safecycle-client/src/app/feature/itinerary/itinerary.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-itinerary',
   templateUrl: './itinerary.component.html',
   styleUrls: ['./itinerary.component.scss']
 })
-export class ItineraryComponent implements OnInit {
+export class ItineraryComponent implements OnInit, AfterViewInit {
+
+  // @ts-ignore
+  public map;
 
   constructor() { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {}
+
+  public ngAfterViewInit(): void {
+    this.initMat();
+  }
+
+
+
+  private initMat(): void {
+    this.map = L.map('map', {
+      center: [ 48.858840058739816, 7.073708905917356 ],
+      zoom: 18,
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      detectRetina: true
+    });
+
+    tiles.addTo(this.map);
   }
 
 }
