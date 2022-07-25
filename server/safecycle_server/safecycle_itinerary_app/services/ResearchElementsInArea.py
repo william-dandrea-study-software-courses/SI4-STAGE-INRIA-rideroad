d3ec5900af6162c95bc1fb606e16873274834f42
William D'Andrea <williapile@gmail.com>
2022-07-25 14:34:26 +0200

    Début de l'implémentation du service de recherches de points stratégique grace à la library OverPass de OpenStreetMap et la library OSMPythonTools

--- server/safecycle_server/safecycle_itinerary_app/services/ResearchElementsInArea.py
+++ server/safecycle_server/safecycle_itinerary_app/services/ResearchElementsInArea.py
@@ -1,6 +1,8 @@
 from OSMPythonTools.overpass import overpassQueryBuilder
 from OSMPythonTools.overpass import Overpass
 
+from server.safecycle_server.safecycle_itinerary_app.services.models.AmenityEnum import AmenityEnum
+
 """
     - Points d'eau  =>  selector='"amenity"="drinking_water"'
     - Campings      =>  
@@ -9,10 +11,19 @@ from OSMPythonTools.overpass import Overpass
 
 class ResearchElementsInArea:
 
+    """
+        - Liste de segments autour dequels on va chercher des trucs
+        - Rayon autour duquel on va chercher des lieus
+        - List de tout les lieux (AmenityEnum) que l'on va chercher
+    """
+
     def __init__(self):
-        query = overpassQueryBuilder(bbox=[48.27, 16.47, 48.3, 16.5], elementType='node', selector='"amenity"="drinking_water"', out='body')
+        query = overpassQueryBuilder(bbox=[48.27, 16.47, 48.3, 16.5], elementType='node', selector=[f'"amenity"="{AmenityEnum.DRINKING_WATER}"'], out='body')
 
         overpass = Overpass()
         busStops = overpass.query(query)
 
         print(busStops.toJSON())
+
+
+
