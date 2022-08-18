ce5f8ccef4c5e96a2a41310daf64539c5a82fec8
William D'Andrea <williapile@gmail.com>
2022-08-18 16:13:41 +0200

    Envoi des elements à OSM

--- server/safecycle_server/safecycle_itinerary_app/services/ContributeAmenities.py
+++ server/safecycle_server/safecycle_itinerary_app/services/ContributeAmenities.py
@@ -3,17 +3,58 @@ import osmapi
 
 class ContributeAmenities:
 
+    URL_API: str = "https://master.apis.dev.openstreetmap.org"
+
     def __init__(self):
         pass
 
     def addNewDrinkingWater(self, longitude: float, latitude: float, login: str, password: str, access: str, fee: str):
-        api = osmapi.OsmApi(username=login, password=password)
+        api = osmapi.OsmApi(username=login, password=password, api=self.URL_API)
+
+        api.ChangesetCreate({"comment": login + " modification"})
+        creationResult = api.NodeCreate({"lon": longitude, "lat": latitude, "tag": {
+            "amenity": "drinking_water",
+            "access": access,
+            "fee": fee,
+        }})
+        print(creationResult)
+        api.ChangesetClose()
 
     def addNewRepairStation(self, longitude: float, latitude: float, login: str, password: str, fee: str, attribute_repair: str, attribute_pump: str, attribute_tools: str):
-        api = osmapi.OsmApi(username=login, password=password)
+        api = osmapi.OsmApi(username=login, password=password, api=self.URL_API)
 
-    def addNewShelter(self, longitude: float, latitude: float, login: str, password: str, bench: str, bin: str):
-        api = osmapi.OsmApi(username=login, password=password)
+        api.ChangesetCreate({"comment": login + " modification"})
+        creationResult = api.NodeCreate({"lon": longitude, "lat": latitude, "tag": {
+            "amenity": "bicycle_repair_station",
+            "service:bicycle:repair": attribute_repair,
+            "service:bicycle:pump": attribute_pump,
+            "service:bicycle:tools": attribute_tools,
+            "fee": fee,
+        }})
+        print(creationResult)
+        api.ChangesetClose()
+
+    def addNewShelter(self, longitude: float, latitude: float, login: str, password: str, bench: str, bin_v: str):
+        api = osmapi.OsmApi(username=login, password=password, api=self.URL_API)
+
+        api.ChangesetCreate({"comment": login + " modification"})
+        creationResult = api.NodeCreate({"lon": longitude, "lat": latitude, "tag": {
+            "amenity": "shelter",
+            "bench": bench,
+            "bin": bin_v,
+        }})
+        print(creationResult)
+        api.ChangesetClose()
 
     def addNewToilets(self, longitude: float, latitude: float, login: str, password: str, disposal: str, access: str, gender: str ):
-        api = osmapi.OsmApi(username=login, password=password)
+        api = osmapi.OsmApi(username=login, password=password, api=self.URL_API)
+
+        api.ChangesetCreate({"comment": login + " modification"})
+        creationResult = api.NodeCreate({"lon": longitude, "lat": latitude, "tag": {
+            "amenity": "toilets",
+            "disposal": disposal,
+            "access": access,
+            "gender": gender,
+        }})
+        print(creationResult)
+        api.ChangesetClose()
