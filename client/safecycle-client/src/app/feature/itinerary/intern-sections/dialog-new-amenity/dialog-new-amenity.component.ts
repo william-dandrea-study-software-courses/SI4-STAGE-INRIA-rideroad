5db4398c8cf23d35a5c5932dc7a1759517f1dbf7
William D'Andrea <williapile@gmail.com>
2022-08-18 09:45:22 +0200

    HTML pour la partie "ajout de nouvelle amenities"

--- client/safecycle-client/src/app/feature/itinerary/intern-sections/dialog-new-amenity/dialog-new-amenity.component.ts
+++ client/safecycle-client/src/app/feature/itinerary/intern-sections/dialog-new-amenity/dialog-new-amenity.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {EditAmenityService} from "../../../../core/service/edit-amenity.service";
 import {AmenityTitle} from "../../../../core/service/amenity.service";
 import {FormControl} from "@angular/forms";
+import {AmenityEnum} from "../../../../core/model/amenity.model";
 
 @Component({
   selector: 'app-dialog-new-amenity',
@@ -10,18 +11,26 @@ import {FormControl} from "@angular/forms";
 })
 export class DialogNewAmenityComponent implements OnInit {
 
-  public editableAmenities: AmenityTitle[] = [];
+  public bikeServicesRepairStation: {name: string, isSelected: boolean, attribute: string, value: string}[] = [
+    {name: 'Reparation', isSelected: true, attribute: "service:bicycle:repair", value: "no"},
+    {name: 'Pump', isSelected: true, attribute: "service:bicycle:pump", value: "no"},
+    {name: 'Tools', isSelected: true, attribute: "service:bicycle:tools", value: "no"},
+  ];
 
-  public myControl = new FormControl('');
+
 
   constructor(private editAmenityService: EditAmenityService) { }
 
   ngOnInit(): void {
-    this.editAmenityService.$editableAmenitiesTitle.subscribe(amenitiesEditable => {
-      this.editableAmenities = amenitiesEditable;
-    })
+
   }
 
+
+  public onSelectRepairStation(value: {name: string, isSelected: boolean, attribute: string, value: string}) {
+    value.isSelected = !value.isSelected;
+    value.value = value.isSelected ? "yes" : "no";
+
+  }
 
   public onValidate() {
     console.log("Validate")
--- client/safecycle-client/src/environments/environment.ts
+++ client/safecycle-client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  backend_url: "http://127.0.0.1:8080/api/",
+  backend_url: "http://127.0.0.1:8000/api/",
 };
 
 /*
