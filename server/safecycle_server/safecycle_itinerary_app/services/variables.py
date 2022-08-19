586cefa4bb88e5fbe374023bcf7468efa2f6b1b3
julien deantoni <julien.deantoni@inria.fr>
2022-08-19 14:34:28 +0200

    minor refactoring and cleaning

--- server/safecycle_server/safecycle_itinerary_app/services/variables.py
+++
@@ -1,8 +0,0 @@
-
-
-URL_ADD_NEW_AMENITIES: str = "https://master.apis.dev.openstreetmap.org"
-
-BASE_ROUTING_INFOS_OSM_URL: str = "https://routing.openstreetmap.de/routed-bike/route/v1/driving/"
-DETAILS_ROUTING_INFOS_OSM_URL: str = "?overview=false&alternatives=false&steps=true"
-
-BROUTER_BASE_URL: str = "http://brouter.de/brouter?"
